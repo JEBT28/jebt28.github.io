d1812ed9e080a917f117e4e7fb157a2c27edbb2e
Juan Baltierrez <juanbal2808@gmail.com>
2021-01-19 15:02:40 -0600

    Pagina de inicio resposive

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+function desplegarNavBar() {
+    const lateralnavbar = document.getElementById('lateralnavbar');
+    lateralnavbar.style.display ='block';
+}
+
+function esconderNavBar(){
+    const lateralnavbar = document.getElementById('lateralnavbar');
+    lateralnavbar.style.display ='none';
+}
+
+
+
+
+
+
+
+
+
